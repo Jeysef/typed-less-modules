aecb2d06ce173e3ef79542d60cdce47edc60fcf2
Jeysef <josimich7@gmail.com>
2023-11-30 15:44:32 +0100

    fix: correct style type

--- examples/default-export/feature-a/style.less.d.ts
+++ examples/default-export/feature-a/style.less.d.ts
@@ -1,2 +1,11 @@
-export declare const i: string;
-export declare const iAmKebabCased: string;
+export type Styles = {
+  i: string;
+  "i-am-kebab-cased": string;
+  while: string;
+};
+
+export type ClassNames = keyof Styles;
+
+declare const styles: Styles;
+
+export default styles;
